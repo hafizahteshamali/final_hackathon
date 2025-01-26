6d787dc0aa71fd0b7e42a26e080176b32a541177
hafizahteshamali <hafizahteshamali363617@gmail.com>
2025-01-26 05:54:18 +0500

    task_completed

--- Frontend/src/Api/index.js
+++ Frontend/src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ApiClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://final-hackathon-git-main-ahteshamalis-projects.vercel.app/api",
     timeout: 30000,
     headers:{
         "Content-Type": "application/json"
